236299feb511de81ee4311b4cc3e7307aafea4c2
reardek <jjkowalinski@yahoo.com>
2020-10-15 11:22:58 +0200

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+    </div>
+  );
+}
+
+export default App;
