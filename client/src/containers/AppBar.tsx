8b5b1b0f3e170b9035e9335654bb81e10fa85c79
reardek <jjkowalinski@yahoo.com>
2021-03-11 17:42:59 +0100

    Testing Formik

--- client/src/containers/AppBar.tsx
+++ client/src/containers/AppBar.tsx
@@ -1,223 +1,155 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Link as RouterLink,
-  Switch,
-  Route,
-} from "react-router-dom";
-import clsx from "clsx";
-import {
   makeStyles,
   useTheme,
   Theme,
   createStyles,
 } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
-import Link from "@material-ui/core/Link";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
+import { Button, Card, IconButton, LinearProgress } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
-import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
-import QuestionTab from "./QuestionsTab";
-import { Button } from "@material-ui/core";
-
-const drawerWidth = 180;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route,
+  useHistory,
+  LinkProps,
+} from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-material-ui";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: "flex",
     },
-    appBar: {
-      transition: theme.transitions.create(["margin", "width"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
+    contenet: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
     },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create(["margin", "width"], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
+    toolbar: theme.mixins.toolbar,
     menuButton: {
       marginRight: theme.spacing(2),
     },
-    hide: {
-      display: "none",
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-      color: "inherit",
-    },
-    drawerHeader: {
-      display: "flex",
-      alignItems: "center",
-      padding: theme.spacing(0, 1),
-      // necessary for content to be below app bar
-      ...theme.mixins.toolbar,
-      justifyContent: "flex-end",
-    },
-    content: {
+    title: {
       flexGrow: 1,
-      padding: theme.spacing(3),
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginLeft: -drawerWidth,
     },
-    contentShift: {
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    },
-    listItem: {
-      color: "inherit",
+    card: {
+      display: "inline-block",
+      minWidth: "3em",
+      alignItems: "center",
+      padding: theme.spacing(2),
     },
   })
 );
 
+const LinkBehavior = React.forwardRef<any, Omit<LinkProps, "to">>(
+  (props, ref) => <Link ref={ref} to="/protected" {...props} />
+);
+
+interface Values {
+  email: string;
+  password: string;
+}
+
 export default function PersistentDrawerLeft() {
   const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Persistent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Router>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "ltr" ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
+    <Router>
+      <div>
+        <CssBaseline />
+
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
             </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit" component={LinkBehavior}>
+              SignUp
+            </Button>
+            <Button color="inherit" component={LinkBehavior}>
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <main className={classes.contenet}>
+          <div className={classes.toolbar}>
+            <Switch>
+              <Route path="/protected">
+                <Card className={classes.card}>
+                  <Formik
+                    initialValues={{
+                      email: "",
+                      password: "",
+                    }}
+                    validate={(values) => {
+                      const errors: Partial<Values> = {};
+                      if (!values.email) {
+                        errors.email = "Required";
+                      } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+                          values.email
+                        )
+                      ) {
+                        errors.email = "Invalid email address";
+                      }
+                      return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                      setTimeout(() => {
+                        setSubmitting(false);
+                        alert(JSON.stringify(values, null, 2));
+                      }, 500);
+                    }}
+                  >
+                    {({ submitForm, isSubmitting }) => (
+                      <Form>
+                        <Field
+                          component={TextField}
+                          name="email"
+                          type="email"
+                          label="Email"
+                        />
+                        <br />
+                        <Field
+                          component={TextField}
+                          type="password"
+                          label="Password"
+                          name="password"
+                        />
+                        {isSubmitting && <LinearProgress />}
+                        <br />
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          disabled={isSubmitting}
+                          onClick={submitForm}
+                        >
+                          Submit
+                        </Button>
+                      </Form>
+                    )}
+                  </Formik>
+                </Card>
+              </Route>
+            </Switch>
           </div>
-          <Divider />
-          <List>
-            <Link
-              component={RouterLink}
-              to="/questions"
-              underline="none"
-              className={classes.listItem}
-            >
-              <ListItem button>
-                <ListItemIcon>
-                  <HelpOutlineIcon />
-                </ListItemIcon>
-                <ListItemText>Pytania</ListItemText>
-              </ListItem>
-            </Link>
-            <Link
-              component={RouterLink}
-              to="/feedback"
-              underline="none"
-              className={classes.listItem}
-            >
-              {" "}
-              <ListItem button>
-                <ListItemIcon>
-                  <EmojiEmotionsIcon />
-                </ListItemIcon>
-                <ListItemText>Feedback</ListItemText>
-              </ListItem>
-            </Link>
-          </List>
-        </Drawer>
-        <main
-          className={clsx(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >
-          <div className={classes.drawerHeader} />
-          <Switch>
-            <Route path="/questions">
-              <QuestionTab></QuestionTab>
-            </Route>
-            <Route path="/feedback">
-              <Button>Increase</Button>
-              <Typography paragraph>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-                ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-                elementum integer enim neque volutpat ac tincidunt. Ornare
-                suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-                volutpat consequat mauris. Elementum eu facilisis sed odio
-                morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in.
-                In hendrerit gravida rutrum quisque non tellus orci ac.
-                Pellentesque nec nam aliquam sem et tortor. Habitant morbi
-                tristique senectus et. Adipiscing elit duis tristique
-                sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis.
-                Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-              </Typography>
-            </Route>
-            <Route path="/question1">
-              <Typography paragraph>To jest pytanie</Typography>
-            </Route>
-          </Switch>
         </main>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
